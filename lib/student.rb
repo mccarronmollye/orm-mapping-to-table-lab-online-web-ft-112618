89f2ff189d32d1dd5c3221bf0d6b02212bfa1d0a
Molly McCarron <mccarronmollye@gmail.com>
2019-01-10 16:02:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,12 @@
 class Student
 
-  attr_accessor :id 
+  attr_accessor :id, :grade
 
-  def intitialize (id:)
+  def intitialize (id:, grade:)
     @id = id
+    @grade = grade
   end 
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
