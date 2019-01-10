2a3b6df10f40b59653a25e27d6fd7fba0293a0c9
Molly McCarron <mccarronmollye@gmail.com>
2019-01-10 16:25:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
    def self.create(name:, grade:)
      student = Student.new(name, grade)
      student.save
-     student 
+     student #to return the record we just created
    end
     
   
