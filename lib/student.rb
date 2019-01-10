90f6de126686012691e547b18e117a3c841b0da9
Molly McCarron <mccarronmollye@gmail.com>
2019-01-10 16:07:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :id
 
   def initialize (id: nil)
-    @id = id
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
   
   # Remember, you can access your database connection anywhere in this class
